8509f6763a6bdc3af14bbebca480da7b68a094a4
yukendev <yuken0906dev@gmail.com>
2024-05-03 20:48:50 +0900

    feat: category color

--- src/model/category/const.ts
+++ src/model/category/const.ts
@@ -1,1 +1,8 @@
+import type { Category } from "./type";
+
 export const categories = ["tech", "book", "note"] as const;
+export const categoryColors: { [K in Category]: string } = {
+  tech: "green",
+  book: "red",
+  note: "blue",
+};
