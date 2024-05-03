bc9b426539d730cfaf097ec061662c00b85caea0
yukendev <yuken0906dev@gmail.com>
2024-05-03 20:48:24 +0900

    feat: category icon component

---
+++ src/model/category/component/category-icon/category-icon.tsx
@@ -0,0 +1,30 @@
+import type { Category } from "@/model/category/type";
+import { rem } from "@mantine/core";
+import { IconBook, IconDevicesPc, IconPencil } from "@tabler/icons-react";
+
+type Props = {
+  category: Category;
+  size: number;
+  color: string;
+};
+
+export const CategoryIcon = ({ category, size, color }: Props) => {
+  if (category === "tech")
+    return (
+      <IconDevicesPc
+        style={{ width: rem(size), height: rem(size) }}
+        color={color}
+      />
+    );
+  if (category === "book")
+    return (
+      <IconBook style={{ width: rem(size), height: rem(size) }} color={color} />
+    );
+  if (category === "note")
+    return (
+      <IconPencil
+        style={{ width: rem(size), height: rem(size) }}
+        color={color}
+      />
+    );
+};
