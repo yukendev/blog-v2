1be45e040eac77b8ee3b7d7d6fcd6acc24b87515
yukendev <yuken0906dev@gmail.com>
2024-05-03 20:48:39 +0900

    feat: blog card component

--- src/model/blog-article/components/blog-card/blog-card.stories.tsx
+++ src/model/blog-article/components/blog-card/blog-card.stories.tsx
@@ -9,10 +9,29 @@ const meta: Meta<typeof BlogCard> = {
 export default meta;
 type Story = StoryObj<typeof BlogCard>;
 
-export const Default: Story = {
+export const Tech: Story = {
   args: {
     title: "【Swift】『static』キーワードの意味・便利な使い方",
     date: "2021-07-08",
     tags: ["Swift", "Next.js"],
+    category: "tech",
+  },
+};
+
+export const Book: Story = {
+  args: {
+    title: "『Clean Architecture』を読んだ",
+    date: "2022-10-30",
+    tags: ["技術書"],
+    category: "book",
+  },
+};
+
+export const Note: Story = {
+  args: {
+    title: "ブログを新しく作り直した",
+    date: "2022-07-10",
+    tags: ["ブログ", "Next.js"],
+    category: "note",
   },
 };
--- src/model/blog-article/components/blog-card/blog-card.tsx
+++ src/model/blog-article/components/blog-card/blog-card.tsx
@@ -1,21 +1,30 @@
 import type { BlogArticle } from "@/model/blog-article/type";
+import { CategoryBadge } from "@/model/category/component/category-badge/category-badge";
+import { categoryColors } from "@/model/category/const";
 import { Badge, Card, Group, Space, Text } from "@mantine/core";
 import Link from "next/link";
 
 type Props = Pick<BlogArticle, "title" | "date" | "tags" | "category">;
 
-export const BlogCard = ({ title, date, tags }: Props) => (
+export const BlogCard = ({ title, date, tags, category }: Props) => (
   <Card
     shadow="sm"
-    w="100%"
     withBorder
     radius="md"
     padding="lg"
     component={Link}
     href=""
     target="_blank"
+    styles={{
+      root: {
+        borderLeft: `medium solid var(--mantine-color-${categoryColors[category]}-filled)`,
+      },
+    }}
   >
-    <Text size="sm">{date}</Text>
+    <Group>
+      <CategoryBadge category={category} size={40} radius="md" />
+      <Text size="sm">{date}</Text>
+    </Group>
     <Space h="sm" />
     <Text fw={700} size="lg">
       {title}
@@ -25,7 +34,7 @@ export const BlogCard = ({ title, date, tags }: Props) => (
       {tags.map((tag) => (
         <Badge
           key={tag}
-          color="cyan"
+          color={categoryColors[category]}
           component={Link}
           href="/hoge"
           target="_blank"
