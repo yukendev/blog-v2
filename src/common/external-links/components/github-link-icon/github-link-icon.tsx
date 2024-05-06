2e7230e8a2a595344c3e258b584d6aefcb182c11
yukendev <yuken0906dev@gmail.com>
2024-05-06 15:35:52 +0900

    feat: add classname props to external link

--- src/common/external-links/components/github-link-icon/github-link-icon.tsx
+++ src/common/external-links/components/github-link-icon/github-link-icon.tsx
@@ -5,14 +5,16 @@ import { github_link } from "../../const";
 type Props = {
   size: number;
   color: string;
+  className?: string;
 };
 
-export const GithubLinkIcon = ({ size, color }: Props) => (
+export const GithubLinkIcon = ({ size, color, className }: Props) => (
   <a href={github_link} target="_blank" rel="noreferrer">
     <Center>
       <IconBrandGithub
         style={{ width: rem(size), height: rem(size) }}
         color={color}
+        className={className}
       />
     </Center>
   </a>
--- src/common/external-links/components/x-link-icon/x-link-icon.tsx
+++ src/common/external-links/components/x-link-icon/x-link-icon.tsx
@@ -5,14 +5,16 @@ import { x_link } from "../../const";
 type Props = {
   size: number;
   color: string;
+  className?: string;
 };
 
-export const XLinkIcon = ({ size, color }: Props) => (
+export const XLinkIcon = ({ size, color, className }: Props) => (
   <a href={x_link} target="_blank" rel="noreferrer">
     <Center>
       <IconBrandX
         style={{ width: rem(size), height: rem(size) }}
         color={color}
+        className={className}
       />
     </Center>
   </a>
